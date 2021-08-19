e6adfd295f0bc716f052786f7e3a6b3387410323
huangshufeng <2662312100@qq.com>
2021-08-19 20:34:09 +0800

    列表增删

--- test01.py
+++ test01.py
@@ -1,2 +1,8 @@
-import random
-print(random.randint(1,10))
+famousPerson = "Albert Einstein"
+message = "A person who never made a mistake never tried anything new."
+a = famousPerson + "once said." + message
+print("\t",a)
+#除去左边空格，类似rstrip(),strip()
+print(a.lstrip())
+
+
